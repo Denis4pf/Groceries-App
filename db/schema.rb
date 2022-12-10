043d94650c05cae4e41451f59922be1304022494
Denis4pf <denis.onwonga@student.moringaschool.com>
2022-12-10 17:40:16 +0300

    hot fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_143502) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_26_044858) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
@@ -19,6 +22,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_143502) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "link"
+    t.string "cuisine"
+    t.string "ingredients"
+    t.string "date"
+    t.string "cook_time"
+    t.boolean "is_favorite"
+    t.boolean "has_made"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
