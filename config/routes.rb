f0a1b4725a2c264e8509c7fa7582949c5e265023
Denis4pf <denis.onwonga@student.moringaschool.com>
2022-12-11 13:06:13 +0300

    fixed cookies

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   get '/me', to: 'users#show'
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
+  get '/hello', to: 'application#hello_world'
+  
 end
